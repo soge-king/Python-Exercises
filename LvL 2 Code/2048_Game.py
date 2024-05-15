d84bee4df53d6476f6bf95850f4ae998a59f972f
Franco Martinie <29467060+soge-king@users.noreply.github.com>
2024-05-15 16:30:28 -0400

    Create 2048_Game.py

---
+++ LvL 2 Code/2048_Game.py
@@ -0,0 +1,43 @@
+import pygame
+import random
+import math
+
+
+
+
+pygame.init()
+FPS = 60
+
+WIDTH, HEIGHT = 800,800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BAKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+
+if __name__ == "__main__":
+    main(WINDOW)
+
